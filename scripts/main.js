f5e2a918b491e86c427d7ecd454099003bc59c65
odoi21 <solotecladosinl2@gmail.com>
2024-12-05 11:47:49 -0300

    new

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+alert ("Hola, Bienvenido a NoteNest esta es una app web de notas interactivas donde podras crear tus notas personales. Proximamente: Aparicion visual de cada nota, Eliminar, Editar y mcuhas funcionalidades mas")
+
+let = prompt ("Ingrese el nombre de su nueva lista de notas");
+
