b60e3432a78b58909e9b065935a74dfc5f80bde6
odoi21 <solotecladosinl2@gmail.com>
2024-12-05 20:28:52 -0300

    variable

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 alert ("Hola, Bienvenido a NoteNest esta es una app web de notas interactivas donde podras crear tus notas personales. Proximamente: Aparicion visual de cada nota, Eliminar, Editar y mcuhas funcionalidades mas")
 
-let NameList = prompt ("Ingrese el nombre de su nueva lista de notas");
+let nameList = prompt ("Ingrese el nombre de su nueva lista de notas");
 
-alert (`${NameList} Es el nuevo nombre de su lista de notas`);
+alert (`${nameList} Es el nuevo nombre de su lista de notas`);
 
