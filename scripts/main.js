d8fbf78dc8b1d43700f37c47c232f85055c601a1
odoi21 <solotecladosinl2@gmail.com>
2025-02-02 00:56:32 -0300

    ruta

--- scripts/main.js
+++ scripts/main.js
@@ -25,7 +25,7 @@ function loadItems() {
 }
 
 function loadAlumnos() {
-    fetch('alumnos.json') 
+    fetch('./alumnos.json') 
         .then(response => {
             if (!response.ok) {
                 throw new Error('Error en la red: ' + response.statusText);
